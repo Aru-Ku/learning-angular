ac8cf920ee4f28a379532a21c1176a98e91b693a
ArunKumar Nadikattu <47110396+mastrero@users.noreply.github.com>
2021-08-20 12:55:56 +0530

    add routing to first-app-ak

--- first-app-ak/src/app/app.module.ts
+++ first-app-ak/src/app/app.module.ts
@@ -7,6 +7,19 @@ import { CalculatorComponent } from './calculator/calculator.component';
 import { SalaryCalculatorComponent } from './salary-calculator/salary-calculator.component';
 import { SalaryCalculatorModel } from './salary-calculator/salary-calculator.modal';
 import { ProductsComponent } from './products/products.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found.component';
+
+const routes: Routes = [
+  // { path: '', component: AppComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'greeter', component: GreeterComponent },
+  { path: 'calculator', component: CalculatorComponent },
+  { path: 'salary-calc', component: SalaryCalculatorComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: '**', redirectTo: '404' },
+  { path: '404', component: NotFoundComponent }
+]
 
 @NgModule({
   declarations: [
@@ -17,7 +30,7 @@ import { ProductsComponent } from './products/products.component';
     ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes)
   ],
   providers: [SalaryCalculatorModel],
   bootstrap: [AppComponent]
